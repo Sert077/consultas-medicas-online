c8b3fd6df27783644d09c0b9544541b81e4da5d1
Sert077 <servesa07@gmail.com>
2025-02-12 01:34:25 -0400

    filtros pero falta estilos

--- frontend/src/components/MisReservas.js
+++ frontend/src/components/MisReservas.js
@@ -6,38 +6,38 @@ import '../css/MisReservas.css';
 
 const MisReservas = () => {
     const [reservas, setReservas] = useState([]);
+    const [tipoConsulta, setTipoConsulta] = useState(''); // Filtro por tipo de consulta
+    const [fechaConsulta, setFechaConsulta] = useState(''); // Filtro por fecha
+
     const navigate = useNavigate();
-    const token = localStorage.getItem('token'); // Obtener el token
-    const tipoUsuario = localStorage.getItem('tipo_usuario'); // Obtener el tipo de usuario
-    const userId = localStorage.getItem('paciente_id'); // Obtener el id del usuario logueado
+    const token = localStorage.getItem('token'); 
+    const tipoUsuario = localStorage.getItem('tipo_usuario'); 
+    const userId = localStorage.getItem('paciente_id'); 
 
     useEffect(() => {
         if (!token) {
-            navigate('/login'); // Redirigir si no hay token
+            navigate('/login'); 
             return;
         }
     
-        // Hacer la llamada API dependiendo del tipo de usuario
         if (tipoUsuario === 'medico') {
-            fetch(`http://localhost:8000/api/consultas/medico/${localStorage.getItem('paciente_id')}/`)
+            fetch(`http://localhost:8000/api/consultas/medico/${userId}/`)
                 .then(response => response.json())
                 .then(data => {
-                    console.log('Datos de la API (medico):', data); // Agregar console.log aquí
+                    console.log('Datos de la API (medico):', data);
                     setReservas(data);
                 })
                 .catch(error => console.error('Error fetching reservas:', error));
         } else {
-            // Obtener las consultas del paciente
             fetch(`http://localhost:8000/api/consultas/paciente/${userId}/`)
                 .then(response => response.json())
                 .then(data => {
-                    console.log('Datos de la API (paciente):', data); // Agregar console.log aquí
+                    console.log('Datos de la API (paciente):', data);
                     setReservas(data);
                 })
                 .catch(error => console.error('Error fetching reservas:', error));
         }
     }, [token, navigate, tipoUsuario, userId]);
-    
 
     const handleRealizarConsulta = (consultaId) => {
         navigate(`/chat/${consultaId}`);
@@ -62,38 +62,63 @@ const MisReservas = () => {
     const isRealizarConsultaEnabled = (fecha, hora) => {
         const now = new Date();
         const consultaDate = new Date(`${fecha}T${hora}`);
-        const timeDifference = (consultaDate - now) / 60000; // Diferencia en minutos
-        return timeDifference <= 5 && timeDifference >= -60; // Habilitar 5 min antes hasta 1h después
+        const timeDifference = (consultaDate - now) / 60000;
+        return timeDifference <= 5 && timeDifference >= -60;
     };
+
+    // Filtrar reservas según los filtros seleccionados
+    const reservasFiltradas = reservas.filter(consulta => {
+        return (
+            (tipoConsulta === '' || consulta.tipo_consulta === tipoConsulta) &&
+            (fechaConsulta === '' || consulta.fecha === fechaConsulta)
+        );
+    });
 
     return (
         <div className="reservas-container">
             <h2>Consultas médicas</h2>
-            {reservas.length === 0 ? (
+
+            {/* Filtros */}
+            <div className="filtros-container">
+                <label>Filtrar por tipo de consulta:</label>
+                <select value={tipoConsulta} onChange={(e) => setTipoConsulta(e.target.value)}>
+                    <option value="">Todas</option>
+                    <option value="virtual">Virtual</option>
+                    <option value="presencial">Presencial</option>
+                </select>
+
+                <label>Filtrar por fecha:</label>
+                <input
+                    type="date"
+                    value={fechaConsulta}
+                    onChange={(e) => setFechaConsulta(e.target.value)}
+                />
+            </div>
+
+            {reservasFiltradas.length === 0 ? (
                 <p>No tienes consultas reservadas.</p>
             ) : (
                 <ul className="reservas-list">
-                    {reservas.map(consulta => (
+                    {reservasFiltradas.map(consulta => (
                         <li key={consulta.id}>
                             <div className="fila-superior">
-                            <div className="consulta-id">
-                                <FaHashtag className="icono-id" />
-                                <span><strong>Consulta:</strong> {consulta.id}</span>
+                                <div className="consulta-id">
+                                    <FaHashtag className="icono-id" />
+                                    <span><strong>Consulta:</strong> {consulta.id}</span>
+                                </div>
+                                <div className="estado-consulta">
+                                    <span className={`estado-label ${consulta.estado.toLowerCase()}`}>
+                                        {consulta.estado}
+                                    </span>
+                                </div>
+                                {consulta.estado.toLowerCase() === 'realizada' && consulta.doc_receta && (
+                                    <div className="descarga-receta">
+                                        <a href={`http://localhost:8000${consulta.doc_receta}`} download target="_blank" rel="noopener noreferrer">
+                                            <FaFileDownload className="icono-descarga" /> Ver receta
+                                        </a>
+                                    </div>
+                                )}
                             </div>
-                            <div className="estado-consulta">
-                                <span className={`estado-label ${consulta.estado.toLowerCase()}`}>
-                                    {consulta.estado}
-                                </span>
-                            </div>
-                            {consulta.estado.toLowerCase() === 'realizada' && consulta.doc_receta && (
-                            <div className="descarga-receta">
-                                <a href={`http://localhost:8000${consulta.doc_receta}`} download target="_blank" rel="noopener noreferrer">
-                                    <FaFileDownload className="icono-descarga" /> Ver receta
-                                </a>
-                            </div>
-                        )}
-
-                        </div>
                             <div className="reserva-info">
                                 <FaUserMd className="icono-doctor" />
                                 <p><strong>{tipoUsuario === 'medico' ? 'Paciente:' : 'Doctor:'}</strong> {tipoUsuario === 'medico' ? consulta.paciente_name : consulta.medico_name}</p>
@@ -106,7 +131,9 @@ const MisReservas = () => {
                                 <FaClock className="icono-reloj" />
                                 <p><strong>Hora:</strong> {consulta.hora}</p>
                             </div>
-
+                            <div className="reserva-info">
+                                <strong>Tipo de consulta:</strong> {consulta.tipo_consulta}
+                            </div>
                             <div className="divider">
                                 {consulta.archivo_pdf && (
                                     <div className="descarga-pdf">
@@ -116,11 +143,9 @@ const MisReservas = () => {
                                     </div>
                                 )}
                             </div>
-
                             <div className="button-group">
                                 <button
                                     onClick={() => handleRealizarConsulta(consulta.id)}
-                                    
                                 >
                                     Realizar consulta
                                 </button>
